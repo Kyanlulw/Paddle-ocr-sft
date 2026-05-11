bad7f44c1f8a7e2682b18b64a4fc9501f672089f
Kyanlulw <datlc81@gmail.com>
2026-05-11 11:10:53 +0700

    minor error

--- train_mixed30k.sh
+++ train_mixed30k.sh
@@ -13,7 +13,6 @@ python sft_paddleocr_vl.py \
     --train_annotation_path /kaggle/input/datasets/trnlqung/qwen-scored-9300samples/unsorted/mixed30k.json \
     --dataset1_image_root /kaggle/input/datasets/trnlqung/vitext-vqa/ViTextVQA_images/st_images \
     --dataset2_image_root /kaggle/input/datasets/trnlqung/openvivqa/images/images \
-    --dataset2_metadata_path /path/to/openvivqa_train_v2.json \
     --max_length 1536 \
     --pad_to_multiple_of 8 \
     --output_dir ./sft_output_mixed30k \
